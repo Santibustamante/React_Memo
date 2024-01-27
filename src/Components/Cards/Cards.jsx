a81855874bd41c16c37ca22978515b34e3d6b39e
Santiago Bustamante <santiagowalterbustamante@gmail.com>
2024-01-27 14:59:11 +0100

    actualizacion

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import cardArray from "./Data";
 import "./Cards.css";
 
 const Cards = () => {
+  const [cards, setCards] = useState(cardArray);
+  let clickadas = [];
+  const flipCard = (ev, card) => {
+    ev.currentTarget.classList.toggle("flip");
+    if (clickadas.length < 2) {
+      clickadas.push(card);
+      console.log(clickadas[0].name);
+      if (clickadas.length===2 && clickadas[0].name === clickadas[1].name && clickadas[0].id !== clickadas[1].id) {
+        const newarray = [...cards].map((card) => {
+          if (card.name === clickadas[0].name || card.name === clickadas[1].name) {
+            card.img = "./exercise-1/tick.svg";
+            console.log(card);
+            return card;
+          }
+          return card;
+        });
+        setCards(newarray);
+      } else if (clickadas.length===2) {
+        setTimeout(()=>{
+          ev.currentTarget.classList.toggle("flip");   
+        },1000)
+      }
 
-  return  (
-    <>
-      <div className="b-grid">
-        {cardArray.map((card, i) => (
-          <div className="card" key={i} onClick={(e)=>{
-            const clickedCard = e.currentTarget
-            clickedCard.classList.toggle('girada')
-            
-
-            console.log(e.currentTarget)}}>
-            <div className="frente">
-              <img src={card.img} alt="" />
-            </div>
-            <div className="reverso">
-              <img src="exercise-1/universe.svg" alt="" />
-            </div>
-          </div>
-          ))}
-      </div>
-    </>
+      if (clickadas.length === 2) {
+        clickadas = [];
+      }
+    }
+    console.log(clickadas);
+  };
+  return (
+    <div className="b-grid">
+      {cards.map((card, i) => (
+        <div
+          className="card"
+          onClick={(e) => {
+            flipCard(e, card);
+          }}
+          key={i}
+        >
+          <img className="front" src={card.img} alt="oli" />
+          <img className="reverse" src="exercise-1/universe.svg" alt="oli" />
+        </div>
+      ))}
+    </div>
   );
 };
 
